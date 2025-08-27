bc7c4da49b85e8df6416d9be45fbb312761825dc
Akash-dev-lab <111779287+Akash-dev-lab@users.noreply.github.com>
2025-08-27 11:11:06 -0500

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
+require('dotenv').config()
 const app = require('./app')
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const generateResponse = require('./src/services/ai.services')
 
 const httpServer = createServer();
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("A user connected.")
+
+  socket.on("disconnected", () => {
+    console.log("A user disconnected.")
+  })
+
+  socket.on("ai-message", async (data) => {
+    console.log("Prompt: ", data.prompt)
+    const response = await generateResponse(data.prompt)
+    console.log(response)
+    socket.emit("ai-response", {response})
+  })
 });
 
 
